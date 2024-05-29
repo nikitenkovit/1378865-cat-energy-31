da6d85ccf9af81062bcda57f862a0a75da0922f9
Виталий Никитенко <nelot@mail.ru>
2024-05-29 18:36:07 +0300

    Добавит webp изображения и сделает рефакторинг

--- source/scripts/example-slider.js
+++ source/scripts/example-slider.js
@@ -1,74 +1,84 @@
-const Node = {
-  container: document.querySelector('.example-slider'),
-  imageBefore: document.querySelector('.example-slider__before'),
-  comparisonLine: document.querySelector('.example-slider__line'),
-  comparisonButton: document.querySelector('.example-slider__button')
-};
+(
+  function () {
+    const Node = {
+      container: document.querySelector('.example-slider'),
+      imageBefore: document.querySelector('.example-slider__before'),
+      comparisonLine: document.querySelector('.example-slider__line'),
+      comparisonButton: document.querySelector('.example-slider__button')
+    };
 
-const borderWidth = 4;
+    const borderWidth = 4;
 
-const moveHandler = (event) => {
-  event.preventDefault();
+    const isScriptNotAvailable = Object.values(Node).some((it) => !it);
 
-  const containerWidth = Node.container.offsetWidth;
-  const containerLeft = Node.container.getBoundingClientRect().left;
-
-  let clientX = event.clientX;
-  let pageX = event.pageX;
-
-  if (event.touches) {
-    clientX = event.touches[0].clientX;
-    pageX = event.touches[0].pageX;
-  }
-
-  const removeHandlers = () => {
-    document.removeEventListener('mousemove', onMouseMove);
-    document.removeEventListener('touchmove', onMouseMove);
-    Node.comparisonButton.onmouseup = null;
-  };
-
-  const shiftX = clientX - Node.comparisonLine.getBoundingClientRect().left;
-
-  const moveAt = (newPageX) => {
-
-    const moveValue = newPageX - containerLeft - shiftX;
-
-    if (moveValue > 0 && moveValue < containerWidth - borderWidth) {
-      Node.comparisonLine.style.left = `${moveValue}px`;
-    } else {
-      removeHandlers();
+    if (isScriptNotAvailable) {
+      return;
     }
 
-    const lineLeft = Node.comparisonLine.offsetLeft;
+    const moveHandler = (event) => {
+      event.preventDefault();
 
-    Node.imageBefore.style.width = `${lineLeft}px`;
-  };
+      const containerWidth = Node.container.offsetWidth;
+      const containerLeft = Node.container.getBoundingClientRect().left;
 
-  moveAt(pageX);
+      let clientX = event.clientX;
+      let pageX = event.pageX;
 
-  Node.comparisonLine.style.transform = 'none';
+      if (event.touches) {
+        clientX = event.touches[0].clientX;
+        pageX = event.touches[0].pageX;
+      }
 
-  function onMouseMove(mouseEvent) {
-    let newPageX = mouseEvent.pageX;
+      const removeHandlers = () => {
+        document.removeEventListener('mousemove', onMouseMove);
+        document.removeEventListener('touchmove', onMouseMove);
+        Node.comparisonButton.onmouseup = null;
+      };
 
-    if (mouseEvent.touches) {
-      newPageX = mouseEvent.touches[0].pageX;
-    }
+      const shiftX = clientX - Node.comparisonLine.getBoundingClientRect().left;
 
-    moveAt(newPageX);
+      const moveAt = (newPageX) => {
+
+        const moveValue = newPageX - containerLeft - shiftX;
+
+        if (moveValue > 0 && moveValue < containerWidth - borderWidth) {
+          Node.comparisonLine.style.left = `${moveValue}px`;
+        } else {
+          removeHandlers();
+        }
+
+        const lineLeft = Node.comparisonLine.offsetLeft;
+
+        Node.imageBefore.style.width = `${lineLeft}px`;
+      };
+
+      moveAt(pageX);
+
+      Node.comparisonLine.style.transform = 'none';
+
+      function onMouseMove(mouseEvent) {
+        let newPageX = mouseEvent.pageX;
+
+        if (mouseEvent.touches) {
+          newPageX = mouseEvent.touches[0].pageX;
+        }
+
+        moveAt(newPageX);
+      }
+
+      document.addEventListener('mousemove', onMouseMove);
+      document.addEventListener('touchmove', onMouseMove);
+
+      Node.comparisonButton.addEventListener('mouseup', removeHandlers);
+      Node.comparisonButton.addEventListener('touchend', removeHandlers);
+    };
+
+    Node.comparisonButton.addEventListener('mousedown', moveHandler);
+    Node.comparisonButton.addEventListener('touchstart', moveHandler);
+
+    window.addEventListener('resize', () => {
+      Node.comparisonLine.style.left = '50%';
+      Node.imageBefore.style.width = '50%';
+    });
   }
-
-  document.addEventListener('mousemove', onMouseMove);
-  document.addEventListener('touchmove', onMouseMove);
-
-  Node.comparisonButton.addEventListener('mouseup', removeHandlers);
-  Node.comparisonButton.addEventListener('touchend', removeHandlers);
-};
-
-Node.comparisonButton.addEventListener('mousedown', moveHandler);
-Node.comparisonButton.addEventListener('touchstart', moveHandler);
-
-window.addEventListener('resize', () => {
-  Node.comparisonLine.style.left = '50%';
-  Node.imageBefore.style.width = '50%';
-});
+)();
--- source/scripts/nav.js
+++ source/scripts/nav.js
@@ -1,22 +1,32 @@
-const Class = {
-  toggle: 'main-navigation__menu-toggle',
-  toggleActive: 'main-navigation__menu-toggle--active',
-  navigationList: 'main-navigation__list',
-  navigationListActive: 'main-navigation__list--active',
-};
-const Node = {
-  menuButton: document.querySelector(`.${Class.toggle}`),
-  navigationList: document.querySelector(`.${Class.navigationList}`)
-};
+(
+  function () {
+    const Class = {
+      toggle: 'main-navigation__menu-toggle',
+      toggleActive: 'main-navigation__menu-toggle--active',
+      navigationList: 'main-navigation__list',
+      navigationListActive: 'main-navigation__list--active',
+    };
+    const Node = {
+      menuButton: document.querySelector(`.${Class.toggle}`),
+      navigationList: document.querySelector(`.${Class.navigationList}`)
+    };
 
-Node.menuButton.classList.remove(Class.toggleActive);
-Node.navigationList.classList.remove(Class.navigationListActive);
+    const isScriptNotAvailable = Object.values(Node).some((it) => !it);
 
-const clickHandler = (event) => {
-  event.preventDefault();
+    if (isScriptNotAvailable) {
+      return;
+    }
 
-  Node.menuButton.classList.toggle(Class.toggleActive);
-  Node.navigationList.classList.toggle(Class.navigationListActive);
-};
+    Node.menuButton.classList.remove(Class.toggleActive);
+    Node.navigationList.classList.remove(Class.navigationListActive);
 
-Node.menuButton.addEventListener('click', clickHandler);
+    const clickHandler = (event) => {
+      event.preventDefault();
+
+      Node.menuButton.classList.toggle(Class.toggleActive);
+      Node.navigationList.classList.toggle(Class.navigationListActive);
+    };
+
+    Node.menuButton.addEventListener('click', clickHandler);
+  }
+)();
